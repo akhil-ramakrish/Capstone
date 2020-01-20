e26042bc40b93eeb2f685d26f0e9dcd8fc15b6b9
akhilrajarj <raj30.akhil@gmail.com>
2020-01-20 21:55:11 +0530

    commit Refactored code

--- index.js
+++ index.js
@@ -1,161 +1,180 @@
-var displayValue="";
-var operator="";
-var result = 0;
-var count=0;
-var flag=true;
-var newResult="";
-var continueCalc="";
+ var operator="";
+ var currentValue="";
+ var  num ="";
+ var calcList=[];
+ var count=0;
+ var formatNumber="";
+ var flag;
+ var currentOperator="";
 
 function getResult(){
-    return document.getElementById('result').innerText;
+  return document.getElementById('result').innerText;
 }
 
 function printResult(value){
-    document.getElementById('result').innerText=value;
-}
-
-function getSubResult(){
-  return document.getElementById('sub-result').innerText;
-}
-
-function printSubResult(value){
-   document.getElementById('sub-result').innerText=value;
+document.getElementById('result').innerText = value;
 }
 
 function getInput(inputValue){
   
-  if(inputValue==='='||inputValue==='+'||inputValue==='-'||inputValue==='x'|| inputValue==='\xf7' || inputValue==='\x25' ){
-            //process accordingly
-            if(inputValue==='=')
-            { 
-              printResult(result);
-              printSubResult("");
-              displayValue="";
-              result=0;
-              flag=true;
-            }
-            else{ 
-                  operator = inputValue;
-                  printResult(getResult()+inputValue);
-                  displayValue=getResult();
-                }
-  
-  }
- else if(inputValue==='C'||inputValue==='CE'){
-      //process accordingly
-      Clear(inputValue);
-  }
-  else{
-    //process accordingly
-      displayValue=displayValue+inputValue;
-      printResult(displayValue);
-      if(result!=0){
-         continueCalc = result+operator+inputValue;
-        var values=continueCalc.split(operator);
-        if(values[0]&&values[1]){
-           newResult=compute(operator,false);
-        }
-        printSubResult(newResult);
+if(inputValue==='='||inputValue==='+'||inputValue==='-'||inputValue==='x'|| inputValue==='\xf7' || inputValue==='\x25' ){
+      count=0;
+      formatNumber="";
+      operator=inputValue;
+      currentOperator=operator;
+      currentValue = currentValue+inputValue;
+      printResult(currentValue);
+      if(inputValue==="="){
+            processInput(currentValue.replace(/,/g,''));
+            console.log(calcList);
+            printResult(compute(calcList).toLocaleString("en"));
+            calcList=[];
+            currentValue=getResult();
+             
       }
-      if(operator&&flag){
-        var values=getResult().split(operator);
-        if(values[0]&&values[1]){
-          printSubResult(compute(operator,true));
-          flag=false;
-        }
-      }
-    
-      
-      
-  }
-  
 }
-
-function Clear(value){
-  if(value==='C'){
-      printResult("");
-      printSubResult("");
-      displayValue="";
-      result=0;
-  }
-  else{
-        var presentText=getResult(); 
-        var newText = presentText.substr(0,presentText.length-1);
-        printResult(newText);
-        displayValue=getResult();
-        var values=getResult().split(operator);
-        if(values[0]&&values[1]){printSubResult(compute(operator));}
-        var lastchar =newText.substr(newText.length-1,1);
-        if(lastchar=="+"||lastchar=="-"||lastchar=="x"||lastchar=="\xf7"||lastchar=="\x25"){
-          printSubResult("");
-        }
-        result=0;
-        flag = true;
-  }
-
+else if(inputValue==='C'||inputValue==='CE'){
+   count=0;
+   formatNumber="";
+   clear(inputValue);
 }
- function compute(operator,first){
-   if(first){
-            console.log(operator);
-            var num1=0;
-            var num2=0;
-            var getValues = getResult().split(operator); 
-        
-            if(getValues[0].includes(".")){ num1 = parseFloat(getValues[0]);}
-            else{num1 = parseInt(getValues[0]);}
-        
-            if(getValues[1].includes(".")){ num2=parseFloat(getValues[1]);}
-            else{num2 = parseInt(getValues[1]);}
-            
-            
-            switch(operator){
-              case '+': result = num1 + num2;
-                        break;
-              case '-':result = num1 - num2;
-                      break;
-              case 'x':result = num1 * num2;
-                        break;
-              case '\xf7':result = num1/num2;
-                          break;
-        
-              case '\x25':result=(num1/100)*num2;
-                          break;
-                default: console.log("error");
+else{ 
+       count++;
+        formatNumber=formatNumber+inputValue;
+       if(count>3){
+            var csNum = Number(formatNumber);
+            if(currentOperator){
+                          
+                          var getIndex = currentValue.indexOf(currentOperator);
+                          var newValue=currentValue.slice(0,getIndex+1);
+                          currentValue = newValue+csNum.toLocaleString("en");
+            } 
+            else{
+                  currentValue = csNum.toLocaleString("en");
             }
-          displayValue=getResult();
-  
-   }
-   else{
-          console.log(operator);
-          var num1=0;
-          var num2=0;
-          var getValues = continueCalc.split(operator); 
-      
-          if(getValues[0].includes(".")){ num1 = parseFloat(getValues[0]);}
-          else{num1 = parseInt(getValues[0]);}
-      
-          if(getValues[1].includes(".")){ num2=parseFloat(getValues[1]);}
-          else{num2 = parseInt(getValues[1]);}
-          
-          
-          switch(operator){
-            case '+': result = num1 + num2;
-                      break;
-            case '-':result = num1 - num2;
-                    break;
-            case 'x':result = num1 * num2;
-                      break;
-            case '\xf7':result = num1/num2;
-                        break;
-      
-            case '\x25':result=(num1/100)*num2;
-                        break;
-              default: console.log("error");
+       }
+       else{
+            if(flag){
+
+                  if(currentOperator){
+                        var getIndex=currentValue.indexOf(currentOperator);
+                        var preValue = currentValue.slice(0,getIndex+1); 
+                        var alteredVal=currentValue.slice(getIndex+1);
+                        if(alteredVal){
+                              var newVal = alteredVal.replace(/,/g,'');
+                              currentValue = preValue+Number(newVal+inputValue).toLocaleString("en");
+                        }
+                        
+                  }
+                  else{
+                        var newNum = currentValue.replace(/,/g,'');
+                        currentValue=Number(newNum+inputValue).toLocaleString("en"); 
+                  }
+                  flag=false;
           }
-        displayValue=getResult();
+          else{currentValue = currentValue+inputValue;}
+            
+       }
+      
+      printResult(currentValue);
 
+}
+
+}
+
+function clear(inputValue){
+  if(inputValue==='C'){
+      currentValue="";
+      operator="";
+      currentOperator="";
+      printResult(currentValue);
+      calcList=[];
+  }
+  else{
+         var deleteLastChar = currentValue.substr(0,currentValue.length-1);
+         currentValue = deleteLastChar;
+         //trace current operator in reverse to find current operator
+         for(i=currentValue.length;i>0;i--){
+                if(currentValue[i]==='+'||currentValue[i]==='-'||currentValue[i]==='x'|| currentValue[i]==='\xf7' || currentValue[i]==='\x25'){
+                   currentOperator=currentValue[i]; 
+                   break;
+                }
+                else{currentOperator='';}
+         }
+
+         if(currentOperator){
+            var indexOfOperator = currentValue.indexOf(currentOperator);
+            var preValue = currentValue.slice(0,indexOfOperator+1);
+            var alteredValue = currentValue.slice(indexOfOperator+1);
+            if(alteredValue){
+                  var displayCorrectFormat = alteredValue.replace(/,/g,'');
+                  var format_To_Num = Number(displayCorrectFormat).toLocaleString("en");
+                  currentValue=preValue+format_To_Num;
+            }
+
+            
+         }
+         else{    
+                 var newNum = currentValue.replace(/,/g,'');
+                 currentValue=Number(newNum).toLocaleString("en");
+         }
+         printResult(currentValue);
+         flag=true;
+         calcList=[];
+  }
+}
+
+
+function processInput(processValue){
+   for(var i=0;i<processValue.length;i++){
+         if(processValue[i]==='='||processValue[i]==='+'||processValue[i]==='-'||processValue[i]==='x'|| processValue[i]==='\xf7' ||processValue[i]==='\x25'){
+               operator=processValue[i];
+               if(num){
+                   calcList.push(Number(num));
+                   calcList.push(operator);
+                   num="";
+               }
+
+         }
+         else{
+                num = num + processValue[i];
+         }
    }
-   return result;
- }
+}
 
 
+function compute(calcdata){
+      var calc_operator="";
+      var num1 =0;
+      var num2=0;
+      var result=0;
+      var index=0;
+    for(var i=0;i<calcdata.length;i++){
+          if(calcdata[i]==="="){
+                return result;
+          }
+          if(calcdata[i]==='+'||calcdata[i]==='-'||calcdata[i]==='x'|| calcdata[i]==='\xf7' ||calcdata[i]==='\x25'){
+                calc_operator = calcdata[i];
+                num1 = calcdata[i-1];
+                num2 = calcdata[i+1];
+                index = i+1;
+                switch(calc_operator){
+                  case '+':result=num1+num2;
+                           break;
+                  case '-':result=num1-num2;
+                            break;
+                  case 'x': result = num1*num2;
+                            break;
+                  case '\xf7':result = num1/num2;
+                               break;
+                  case '\x25':result=(num1/100)*num2;
+                              break;
+                  default: console.log("error");
+                }
+                calcdata[index]=result;
+          }
+         
+          
+
+    }
+}
